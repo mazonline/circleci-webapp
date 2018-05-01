0a31be19cd00caae660f6f4626c4a80f8568317c
mazonline <mazonline@users.noreply.github.com>
2018-05-01 09:04:23 -0700

    Update test_button_click.py

--- tests/test_button_click.py
+++ tests/test_button_click.py
@@ -14,7 +14,7 @@ class NewTest(unittest.TestCase):
         self.selenium.start()
         # We instantiate and start the browser
 
-    def test_new(self):
+    #def test_new(self):
         # This is the test code.  Here you should put the actions you need
         # the browser to do during your test.
 
@@ -30,7 +30,7 @@ class NewTest(unittest.TestCase):
         #self.failUnless(sel.is_text_present("Results * for selenium rc"))
         # These are the real test steps
     
-    def test_button_click()
+ test_new(self):
         sel.open("/")
         #test button click of mazonline.github.io/index.html
         element = driver.find_element_by_xpath("//button[@type='myFunction']").click()
