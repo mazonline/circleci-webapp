2b74879d4476aacd701525ef4e5d28d83a41d700
mazonline <mazonline@users.noreply.github.com>
2018-05-01 10:25:03 -0700

    Update test_button_click.py

--- tests/test_button_click.py
+++ tests/test_button_click.py
@@ -7,16 +7,13 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    def test_search_in_python_org(self):
+    def test_button_click_in_site(self):
         driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
-
-
+        driver.get("http://mazonline.github.io/index.html")
+        elem = driver.find_element_by_name("answer")
+        clicked_button = elem.click()
+        print 'Button has been clicked and there is text'
+     
     def tearDown(self):
         self.driver.close()
 
