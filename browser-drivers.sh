8361e1485cbb77e4fd1dcbd19d61c7af61b93cbf
mazonline <mazonline@users.noreply.github.com>
2018-04-30 17:29:48 -0700

    Update browser-drivers.sh

--- browser-drivers.sh
+++ browser-drivers.sh
@@ -4,5 +4,8 @@ sudo mkdir /work
 cd /wprk
 wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux32.tar.gz 
 tar xfvz geckodriver-v0.20.1-linux32.tar.gz 
+pwd
 chmod +x geckodriver
 sudo mv geckodriver /usr/local/bin
+echo "Confirming geckodriver is in $PATH"
+which geckodriver
