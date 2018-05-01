016a86f49d9ab42623c1422f4bb032194ebf3d89
mazonline <mazonline@users.noreply.github.com>
2018-05-01 10:20:16 -0700

    Update test_button_click.py

--- tests/test_button_click.py
+++ tests/test_button_click.py
@@ -1,21 +1,24 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-class SearchText(unittest.TestCase):
+class PythonOrgSearch(unittest.TestCase):
 
-	def setUp(self):
-        # create a new Firefox session
+    def setUp(self):
         self.driver = webdriver.Firefox()
-        # navigate to the application home page
-        self.driver.get("http://mazonline.github.io.index.html /")
-	
-	def test_search_by_text(self):
-	element = driver.find_element_by_name("answer").click()
-        print ('Button is clicked and contains text!')
-        sel.wait_for_page_to_load("30000")
 
-   	def tearDown(self):
-        # close the browser window
-        self.driver.quit()
-if __name__ == '__main__':
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
     unittest.main()
